4dcd062ed399e753ec94d4886b19808e682f9723
Timothy <timsheppard97@yahoo.com>
2021-11-15 16:29:38 -0500

    part 3

--- ProjectManager/server/routes/product.routes.js
+++ ProjectManager/server/routes/product.routes.js
@@ -2,10 +2,8 @@ const ProductController = require('../controllers/product.controller')
 module.exports = function(app) {
     app.get('/api', ProductController.index);
     app.post('/api/products', ProductController.create);
-    app.get('/api/products', ProductController.getAll)
-    app.get('/api/products/:id', ProductController.getOne)
+    app.get('/api/products', ProductController.getAll);
+    app.get('/api/products/:id', ProductController.getOne);
+    app.put('/api/products/:id', ProductController.updateProduct);
+    app.delete('/api/products/:id', ProductController.deleteProduct);
 }
-
-
-
-//githubs a bitch
